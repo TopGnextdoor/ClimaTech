db93fe8607edda4e6a1ea31d249cd1c32244ed1b
Divvyansh Kudesiaa <escapematrix.irl@gmail.com>
2026-03-11 23:26:43 +0530

    Auth fix

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCxVkSVoliqVaZLdSGbIbe_Lg9L_GL0OFo',
-  authDomain: 'climatech-c7392.firebaseapp.com',
-  projectId: 'climatech-c7392',
-  storageBucket: 'climatech-c7392.firebasestorage.app',
-  messagingSenderId: '89676403864',
-  appId: '1:89676403864:web:e72b78ba941fa406f66ced'
+  apiKey: 'API KEY',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
 };
 
 const app = initializeApp(firebaseConfig);
